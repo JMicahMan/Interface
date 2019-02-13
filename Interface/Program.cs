945324fa5f52b4d92c16586440da0a6564f3b3c9
JMicahMan <j.micah.manzanares@gmail.com>
2019-02-13 16:45:42 -0600

    Add project files.

---
+++ Interface/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public interface ValRef
+    {
+        int V
+        {
+            get;
+            set;
+        }
+    }
+
+
+    class R : ValRef
+    {
+        private int L;
+
+        public R(int V)
+        {
+            L = V;
+        }
+
+
+        public int V
+        {
+            get   
+            {
+                return L;
+            }
+            set
+            {
+                L = value;
+            }
+        }
+        static void Main(string[] args)
+        {
+            void PV(ValRef P)
+            {
+                Console.WriteLine(P.V);
+            }
+        }
+    }
+
+
+
+}
